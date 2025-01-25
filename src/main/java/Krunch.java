6e99eb6d91ee1833cedfec5e74f67af4cc8f2a7b
Terentwc <terencetanwc@gmail.com>
2025-01-25 15:48:42 +0800

    Level 0 Renaming, Greeting/Asking/Farewell

---
+++ src/main/java/Krunch.java
@@ -0,0 +1,22 @@
+public class Krunch {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+
+        //Greet
+        System.out.println("________________________________");
+        System.out.println("Hello! I'm Krunch");
+        //Ask
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+        //Bye
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________");
+        System.exit(0);
+
+    }
+}
