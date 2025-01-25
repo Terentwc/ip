be8f1c3522f51934dfd7bddbb57122f2530dd802
Terentwc <terencetanwc@gmail.com>
2025-01-25 16:24:46 +0800

    Level 1 Personality change, Echo

--- src/main/java/Krunch.java
+++ src/main/java/Krunch.java
@@ -1,22 +1,34 @@
+import java.util.Scanner;
+
 public class Krunch {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
 
         //Greet
-        System.out.println("________________________________");
-        System.out.println("Hello! I'm Krunch");
+        System.out.println("_____________________________________________________________________________");
+        System.out.println("Oh, what a treat. Finally, you've graced me with your presence.");
         //Ask
-        System.out.println("What can I do for you?");
-        System.out.println("________________________________");
+        System.out.println("Name's Krunch. So, what mighty task are we tackling today?");
+        System.out.println("_____________________________________________________________________________");
+
+        while(true) {
+            String UserInput = scanner.nextLine();
+
+            if(UserInput.equalsIgnoreCase("bye")) {
+                System.out.println("Oh, I see how it is. No need to pretend you’ll miss me. Go on, then. Goodbye.");
+                System.out.println("_____________________________________________________________________________");
+                break;
+            } else {
+                System.out.println(UserInput);
+            }
+        }
+
+
+
         //Bye
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________");
-        System.exit(0);
+//        System.out.println("Bye. Hope to see you again soon!");
+//        System.out.println("________________________________");
+//        System.exit(0);
 
     }
 }
